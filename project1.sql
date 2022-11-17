9dd80c5c6ddd810d1fe80c2f68ae05f2d4344cd3
thinhorigami-rio <thinhorigamirio@localhost.localdomain>
2022-11-17 23:09:21 +0700

    add password for staff

--- project1.sql
+++ project1.sql
@@ -117,6 +117,7 @@ CREATE TABLE staff(
     birth date NOT NULL,
     _address NVARCHAR(100) NOT NULL,
     phone_number VARCHAR(100) NOT NULL,
+    _password VARCHAR(100) NOT NULL,
     -- IdGiaoCa UNIQUEIDENTIFIER,
     _state int NOT NULL
 )
